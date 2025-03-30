9950775cb72abaea6030068622c4fe27d9f7ca4b
阿龙 <along-losef@outlook.com>
2025-03-30 11:51:01 +0000

    update for:上传基本文件

---
+++ boot.cpp
@@ -0,0 +1,16 @@
+//Loseftui boot part
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Welcome to ---" << endl;
+    cout << "Loseftui\n     Press enter..."<< endl;
+    if(getchar() == '\n')
+    {
+        //tuidesktop tdktp;
+        //tdktp.into();
+    }
+    return 0;
+}
